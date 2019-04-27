403297c9482b320ce6d438d33c83dd9107cde831
alaning0 <thepoisonapple2004@gmail.com>
2019-04-27 16:06:42 +1200

    Add Multiple Cities Editor basic

--- src/app/multiple-cities-editor/multiple-cities-editor.component.ts
+++ src/app/multiple-cities-editor/multiple-cities-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-multiple-cities-editor',
@@ -7,8 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultipleCitiesEditorComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: [''],
+      cities: this.fb.array([])
+    })
+
+    this.setCities();
+  }
+
+  data = {
+    cities: [
+      {
+        city: "",
+        addressLines: [
+          {
+            addressLine: "",
+          }
+        ]
+      }
+    ]
+  }
+
+  setCities() {
+    let control = <FormArray>this.myForm.controls.cities;
+    this.data.cities.forEach(x => {
+      control.push(this.fb.group({ 
+        city: x.city, 
+        addressLines: this.setAddressLines(x) }))
+    })
+  }
+
+  setAddressLines(x) {
+    let arr = new FormArray([])
+    x.addressLines.forEach(y => {
+      arr.push(this.fb.group({ 
+        addressLine: y.addressLine 
+      }))
+    })
+    return arr;
+  }
+
+  onSubmit() {
+    alert(this.myForm.value);
+  }
+  
   ngOnInit() {
   }
 
